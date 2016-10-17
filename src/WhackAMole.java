ffef4315011a460d11b11c74d87f9fb21453e03f
TJHL <dont fill this in! />
2016-10-16 17:27:11 -0700

    whack a mole and spelling bee

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,19 +1,26 @@
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
 
 	JFrame window = new JFrame();
 	JPanel panel = new JPanel();
-	JButton button = new JButton();
+	int score = 0;
+	int wrong = 0;
+	static Date d;
 
 	public static void main(String[] args) {
 		WhackAMole a = new WhackAMole();
+		d = new Date();
 		a.drawButtons(new Random().nextInt(24));
 	}
 
@@ -22,14 +29,19 @@ public class WhackAMole implements ActionListener {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		for (int i = 0; i < 24; i++) {
 			if (i == x) {
+				JButton button = new JButton();
 				panel.add(button);
 				button.setText("mole!");
+				button.addActionListener(this);
 			} else {
-				panel.add(new JButton());
+				JButton b = new JButton();
+				b.addActionListener(this);
+				panel.add(b);
 			}
 		}
 		window.add(panel);
 		window.setSize(300, 320);
+		window.setTitle("Whack a Button for Fame and Glory");
 	}
 
 	void speak(String words) {
@@ -39,10 +51,46 @@ public class WhackAMole implements ActionListener {
 			e.printStackTrace();
 		}
 	}
+	
+	private void endGame(Date timeAtStart, int molesWhacked) {
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is "
+		+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+		}
+	
+	private void playSound(String fileName) {
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
+		JButton t = (JButton)e.getSource();
+		if (t.getText().equals("mole!")) {
+			score++;
+			wrong = 0;
+			window.dispose();
+			panel.removeAll();
+			drawButtons(new Random().nextInt(24));	
+		} else {
+			wrong++;
+			window.dispose();
+			panel.removeAll();
+			drawButtons(new Random().nextInt(24));
+		}
+		if (score == 10) {
+			endGame(d, score);
+		}
+		if (wrong == 1) {
+			speak("dork");
+		} else if (wrong == 2) {
+			speak("idiot");
+		} else if (wrong == 3) {
+			speak("moron");
+		} else if (wrong == 4) {
+			speak("you're a complete waste of atoms!");
+		}
+		
 	}
 
 }
